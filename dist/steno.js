d08f272cfcc295f174683c1e687f0e9352fa63e9
Kiril Knysh <k.knysh@gmail.com>
2017-02-20 20:27:56 +0100

    🐣 ✍️

---
+++ dist/steno.js
@@ -0,0 +1,128 @@
+let prefix = '';
+let perfPrefix = '[perf]';
+const perfBuffer = [];
+const levels = {
+    error: true,
+    warn: false,
+    info: false,
+    log: false,
+    perf: true
+};
+function applyLogLevel(newLevels) {
+    return Object.assign(levels, newLevels);
+}
+function parseLogLevel(logLevelStr) {
+    const levelsStrs = logLevelStr.split(',');
+    const newLevels = {};
+    const disableAll = levelsStrs[0] === 'none';
+    const enableAll = levelsStrs[0] === 'all';
+    if (disableAll || enableAll) {
+        Object.keys(levels).forEach((level) => {
+            newLevels[level] = enableAll || false;
+        });
+        return newLevels;
+    }
+    levelsStrs.forEach((levelStr) => {
+        if (levelStr.startsWith('+')) {
+            newLevels[levelStr.substring(1)] = true;
+        }
+        else if (levelStr.startsWith('-')) {
+            newLevels[levelStr.substring(1)] = false;
+        }
+    });
+    return newLevels;
+}
+function setLogLevel(logLevelStr) {
+    const newLevels = parseLogLevel(logLevelStr);
+    applyLogLevel(newLevels);
+    return steno;
+}
+function readLogLevelFromArgs() {
+    if (!process || !process.argv) {
+        return null;
+    }
+    const processLogLevelParam = process.argv.find(arg => arg.startsWith('--logLevel'));
+    if (!processLogLevelParam) {
+        return null;
+    }
+    const [, processLogLevel] = processLogLevelParam.split('=');
+    return processLogLevel || null;
+}
+function readLogLevelFromEnv() {
+    if (!process || !process.env) {
+        return null;
+    }
+    return process.env.LOG_LEVEL || null;
+}
+function readInitialLogLevel() {
+    const envLogLevel = readLogLevelFromEnv();
+    if (envLogLevel) {
+        return setLogLevel(envLogLevel);
+    }
+    const argsLogLevel = readLogLevelFromArgs();
+    if (argsLogLevel) {
+        return setLogLevel(argsLogLevel);
+    }
+    return steno;
+}
+function initSteno(namespace, logLevel) {
+    prefix = `[${namespace}]`;
+    perfPrefix = `${prefix}${perfPrefix}`;
+    if (logLevel) {
+        setLogLevel(logLevel);
+    }
+    return steno;
+}
+function error(...args) {
+    if (levels.error) {
+        console.error(prefix, ...args);
+    }
+    return steno;
+}
+function warn(...args) {
+    if (levels.warn) {
+        console.warn(prefix, ...args);
+    }
+    return steno;
+}
+function info(...args) {
+    if (levels.info) {
+        console.info(prefix, ...args);
+    }
+    return steno;
+}
+function log(...args) {
+    if (levels.log) {
+        console.log(prefix, ...args);
+    }
+    return steno;
+}
+function perf(...args) {
+    if (levels.perf) {
+        perfBuffer.push(args);
+    }
+    return steno;
+}
+function flush() {
+    if (!perfBuffer.length) {
+        return;
+    }
+    while (perfBuffer.length) {
+        console.log(perfPrefix, ...perfBuffer.shift());
+    }
+    return steno;
+}
+const steno = {
+    initSteno,
+    setLogLevel,
+    flush,
+    error,
+    warn,
+    info,
+    log,
+    perf
+};
+// read and set initial values
+readInitialLogLevel();
+module.exports = steno;
+//# sourceMappingURL=steno.js.map
---
+++ src/steno.ts
@@ -0,0 +1,185 @@
+interface ILogLevel {
+    error: boolean;
+    warn: boolean;
+    info: boolean;
+    log: boolean;
+    perf: boolean;
+}
+
+interface ISteno {
+    initSteno(namespace: string, logLevel?: string): ISteno;
+    setLogLevel(logLevelStr: string): ISteno;
+    flush(): ISteno;
+    error(args: any[]): ISteno;
+    warn(args: any[]): ISteno;
+    info(args: any[]): ISteno;
+    log(args: any[]): ISteno;
+    perf(args: any[]): ISteno;
+}
+
+let prefix: string = '';
+let perfPrefix: string = '[perf]';
+const perfBuffer: any[] = [];
+const levels: ILogLevel = {
+    error: true,
+    warn: false,
+    info: false,
+    log: false,
+    perf: true
+};
+
+function applyLogLevel(newLevels: ILogLevel): ILogLevel {
+    return Object.assign(levels, newLevels);
+}
+
+function parseLogLevel(logLevelStr: string): ILogLevel {
+    const levelsStrs = logLevelStr.split(',');
+    const newLevels: any = {};
+    const disableAll = levelsStrs[0] === 'none';
+    const enableAll = levelsStrs[0] === 'all';
+
+    if (disableAll || enableAll) {
+        Object.keys(levels).forEach((level) => {
+            newLevels[level] = enableAll || false;
+        });
+
+        return newLevels;
+    }
+
+    levelsStrs.forEach((levelStr) => {
+        if (levelStr.startsWith('+')) {
+            newLevels[levelStr.substring(1)] = true;
+        } else if (levelStr.startsWith('-')) {
+            newLevels[levelStr.substring(1)] = false;
+        }
+    });
+
+    return newLevels;
+}
+
+function setLogLevel(logLevelStr: string): ISteno {
+    const newLevels = parseLogLevel(logLevelStr);
+
+    applyLogLevel(newLevels);
+
+    return steno;
+}
+
+function readLogLevelFromArgs(): string | null {
+    if (!process || !process.argv) {
+        return null;
+    }
+
+    const processLogLevelParam = process.argv.find(arg => arg.startsWith('--logLevel'));
+
+    if (!processLogLevelParam) {
+        return null;
+    }
+
+    const [, processLogLevel] = processLogLevelParam.split('=');
+
+    return processLogLevel || null;
+}
+
+function readLogLevelFromEnv(): string | null {
+    if (!process || !process.env) {
+        return null;
+    }
+
+    return process.env.LOG_LEVEL || null;
+}
+
+function readInitialLogLevel(): ISteno {
+    const envLogLevel = readLogLevelFromEnv();
+
+    if (envLogLevel) {
+        return setLogLevel(envLogLevel);
+    }
+
+    const argsLogLevel = readLogLevelFromArgs();
+
+    if (argsLogLevel) {
+        return setLogLevel(argsLogLevel);
+    }
+
+    return steno;
+}
+
+function initSteno(namespace: string, logLevel?: string) {
+    prefix = `[${namespace}]`;
+    perfPrefix = `${prefix}${perfPrefix}`;
+
+    if (logLevel) {
+        setLogLevel(logLevel);
+    }
+
+    return steno;
+}
+
+function error(...args: any[]) {
+    if (levels.error) {
+        console.error(prefix, ...args);
+    }
+
+    return steno;
+}
+
+function warn(...args: any[]) {
+    if (levels.warn) {
+        console.warn(prefix, ...args);
+    }
+
+    return steno;
+}
+
+function info(...args: any[]) {
+    if (levels.info) {
+        console.info(prefix, ...args);
+    }
+
+    return steno;
+}
+
+function log(...args: any[]) {
+    if (levels.log) {
+        console.log(prefix, ...args);
+    }
+
+    return steno;
+}
+
+function perf(...args: any[]) {
+    if (levels.perf) {
+        perfBuffer.push(args);
+    }
+
+    return steno;
+}
+
+function flush() {
+    if (!perfBuffer.length) {
+        return;
+    }
+
+    while (perfBuffer.length) {
+        console.log(perfPrefix, ...perfBuffer.shift());
+    }
+
+    return steno;
+}
+
+const steno: ISteno = {
+    initSteno,
+    setLogLevel,
+    flush,
+    error,
+    warn,
+    info,
+    log,
+    perf
+};
+
+// read and set initial values
+readInitialLogLevel();
+
+module.exports = steno;
